881b16f4cbecfc4db6afc8623530c54fdea395e6
mohammed maheshwar <mohammad.maheshwar@hbwebsol.com>
2024-05-24 17:11:45 +0530

    integrate dashboard api

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -12,16 +12,24 @@ import {getDashboardDataAction} from '../../store/action';
 import {useQuery} from '@apollo/client';
 import {GET_DASHBOARDDATA} from '../../queries/userQueries';
 import AppLoader from '../components/loader';
+import {AppSettingAction} from '../../store/action/dashboardAction';
+import {formatCurrency} from '../../utils/helper';
 
 const DashboardScreen = ({navigation}) => {
   const dispatch = useDispatch();
-  // const {dashBoardData} = useSelector(state => state.dashboard);
   const {loading, error, data} = useQuery(GET_DASHBOARDDATA);
-  console.log(loading, JSON.stringify(data?.dashboardData?.productCount));
-  // useEffect(() => {
-  //   dispatch(getDashboardDataAction());
-  // }, []);
-  // console.log(JSON.stringify(dashBoardData), 'dbb data');
+  const {currencyOptions, currencySymbol} = useSelector(
+    state => state.dashboard,
+  );
+  console.log(
+    loading,
+    error,
+    JSON.stringify(data?.dashboardData?.productCount),
+  );
+  useEffect(() => {
+    dispatch(AppSettingAction());
+  }, []);
+  // console.log(JSON.stringify(data), 'dbb data');
   if (loading) {
     return <AppLoader />;
   }
@@ -32,28 +40,36 @@ const DashboardScreen = ({navigation}) => {
       <ScrollView>
         <DashbaordCardWrapper>
           <DashbaordCard onPress={() => navigation.navigate('Order')}>
-            <DashbaordCardTitle>Latest Orders</DashbaordCardTitle>
-            <DashbaordCardValue>2</DashbaordCardValue>
+            <DashbaordCardTitle>Total Users</DashbaordCardTitle>
+            <DashbaordCardValue>
+              {data?.dashboardData?.userCount}
+            </DashbaordCardValue>
           </DashbaordCard>
           <DashbaordCard>
-            <DashbaordCardTitle>Total Income</DashbaordCardTitle>
-            <DashbaordCardValue>$100000</DashbaordCardValue>
-          </DashbaordCard>
-          <DashbaordCard onPress={() => navigation.navigate('AllProducts')}>
-            <DashbaordCardTitle>Total Products</DashbaordCardTitle>
+            <DashbaordCardTitle>Total Product</DashbaordCardTitle>
             <DashbaordCardValue>
               {data?.dashboardData?.productCount}
             </DashbaordCardValue>
           </DashbaordCard>
-          <DashbaordCard onPress={() => navigation.navigate('AllCustomers')}>
+          <DashbaordCard onPress={() => navigation.navigate('AllProducts')}>
             <DashbaordCardTitle>Total Customers</DashbaordCardTitle>
             <DashbaordCardValue>
               {data?.dashboardData?.customerCount}
             </DashbaordCardValue>
           </DashbaordCard>
+          <DashbaordCard onPress={() => navigation.navigate('AllCustomers')}>
+            <DashbaordCardTitle>Total Sales</DashbaordCardTitle>
+            <DashbaordCardValue>
+              {formatCurrency(
+                data?.dashboardData?.totalSales,
+                currencyOptions,
+                currencySymbol,
+              )}
+            </DashbaordCardValue>
+          </DashbaordCard>
           <DashbaordCard
             onPress={() =>
-              navigation.navigate('ProductsScreen', {
+              navigation.navigate(false ? 'ProductsScreen' : 'Test', {
                 screen: 'AddProduct',
                 initial: false,
               })
